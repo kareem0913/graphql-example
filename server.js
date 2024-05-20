fb6036fdfd25c23d29c57b31486d221187b728f2
Kareem Zaher <kareem.345@yahoo.com>
2024-05-20 15:13:03 +0300

    graphql example

---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
+// const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const typesArray = loadFilesSync("**/*", {
+  extensions: ["graphql"],
+});
+// const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+const resolversArray = loadFilesSync("**/*", {
+  extensions: ["resolvers.js"],
+});
+
+async function startApolloServer() {
+  const app = express();
+
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  await server.start();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(3000, () => {
+    console.log("Running GraphQL server...");
+  });
+}
+
+startApolloServer();
